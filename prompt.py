95594d9e570a36c6703d9ec288c721a0321b946b
Ali Raza <dejavu87@gmail.com>
2023-11-15 00:12:40 +0500

    Update prompt.py

--- prompt.py
+++ prompt.py
@@ -1,17 +1,28 @@
-# Define the delimiter
+# Define the delimiter for clarity in parsing text
 delimiter = "####"
 
-# Define the system message
+# Refine the system message for enhanced understanding and scope
 system_message = f"""
-You will be provided with a list of texts, each associated with a number. 
-The customer service query will be delimited with \
-{delimiter} characters.
-Your task is to determine if each text/or any sentence in the text contains any form of discrimination related to religion, race, gender, or any other protected category. 
-Please provide the output in JSON format with the keys as the input number and the value as a boolean indicating whether discrimination is found.
+You will be presented with several text snippets, each associated with a unique identifier. 
+Each snippet, separated by {delimiter}, may contain statements or questions related to customer service interactions.
+Your role is to analyze these texts to identify any expressions of discrimination or disharmony, including but not limited to biases based on race, religion, gender, ethnicity, or other protected categories. 
+Output your analysis in JSON format, where each key corresponds to the identifier of a text snippet and the value is a boolean. This boolean should indicate the presence ('true') or absence ('false') of discriminatory content.
 """
 
-# Define a sample input
+# Define a more diverse and inclusive sample input for better testing
 sample_input = f"""
 1. I don't want to hire anyone who is not a native English speaker.
 2. We need a park for all residents of the neighborhood to enjoy.
+3. It's important that our team represents a variety of cultural backgrounds.
+4. I prefer not to work with people from certain countries.
+"""
+
+# Define expected JSON output structure for clarity
+expected_output_structure = """
+{
+    "1": true,  # Discrimination based on language/national origin
+    "2": false, # Inclusive statement, no discrimination
+    "3": false, # Encourages diversity, no discrimination
+    "4": true   # Discrimination based on nationality
+}
 """
